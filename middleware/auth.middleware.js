d64aa1a6eda42ad98dacfe0b6c1e69605ef2df0f
syauqiamiq <syauqiamiq12@gmail.com>
2023-11-09 11:12:19 +0700

    feat(middleware): auth middleware

---
+++ middleware/auth.middleware.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+
+const authMiddleware = (req, res, next) => {
+	const header = req.headers["authorization"];
+	if (typeof header !== "undefined") {
+		const token = header.split(" ")[1];
+		jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, data) => {
+			if (err) {
+				res.sendStatus(403);
+			} else {
+				next();
+			}
+		});
+	} else {
+		res.sendStatus(403);
+	}
+};
+
+module.exports = authMiddleware;
